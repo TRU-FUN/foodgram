e60b59fee13eac556cf1e7788ec2194888c70ee9
Даниил <trufun01@gmail.com>
2025-07-08 15:13:07 +0000

    UserCreateSerializer_fix

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -32,6 +32,7 @@ class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'email', 'first_name', 'last_name', 'password')
+	extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User(
